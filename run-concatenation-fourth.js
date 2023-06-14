73afa9c381da4cd9d50e747ea905ed5a428ae7da
SUContent <134996680+SUContent@users.noreply.github.com>
2023-06-14 13:37:28 +0300

    Add files via upload

---
+++ run-concatenation-fourth.js
@@ -0,0 +1,22 @@
+function runConcatenation() {
+    function concatenateTextAndNumbers(text, numbers) {
+        var sortedText = text.split('').sort().join(''); // Rearrange the letters in alphabetical order
+        var sortedNumbers = numbers.sort(function(a, b) { return b - a; }); // Rearrange the numbers in descending order
+        var result = sortedText + " ";
+        for (var i = 0; i < sortedNumbers.length; i++) {
+            result += sortedNumbers[i].toString();
+            if (i !== sortedNumbers.length - 1) {
+                result += ", ";
+            }
+        }
+        return result;
+    }
+
+    var args = process.argv.slice(2);
+    var text = args[0];
+    var numbers = args.slice(1).map(Number);
+
+    console.log(concatenateTextAndNumbers('We are expecting the new Indiana Jones film soon', [100, 200]));
+}
+
+runConcatenation();
